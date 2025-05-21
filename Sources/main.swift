7345b01de3a95919ddbeb29f9320488f5a51f62b
CleideLustosa <142410141+CleideLustosa@users.noreply.github.com>
2025-05-21 17:46:54 +0000

    Pediu nome e Idade, mostrando resultado na tela

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,8 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-var nome = "Cleide"
-let sobrenome = "Lustosa"
 
-print("Bem-vindo(a), \(nome) \(sobrenome)!")
+print("Digite seu nome:")
+let nome = readLine()
+
+print("Digite sua idade:")
+let idade = readLine()
+
+print("Olá, \(nome!)! Você tem \(idade!) anos.")
 
