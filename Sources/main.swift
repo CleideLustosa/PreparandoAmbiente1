235f38151385d38f4a84a3046fb6a6dafeaea6a4
CleideLustosa <142410141+CleideLustosa@users.noreply.github.com>
2025-05-25 14:06:53 +0000

    Atividade de Entrega - Exerc1

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,58 +1,10 @@
 import Foundation
 
 
-// aula 4
+let raio = Double(readLine()!)!
+let pi = 3.14159
 
-// Condicional
+let area = pi * pow(raio, 2)
 
-//let velocidade = 59
-
-//if velocidade > 60 {
-    //print("Multado, velocidade acima do permitido!")
-//} else if velocidade < 30 {
-    //print("Acelere, sua velocidade está abaixo!")
-//} else {
-    //print("Não multado!")
-    //}
-
-// switch case 
-
-//print("Escolha sua bebida:")
-/let opcaoProduto = readLine()!
-let produto = Int (opcaoProduto)!
-
-switch produto {
-case 1:
-    print("Suco")
-case 2:
-    print("Água")
-case 3:
-    print("Refrigerante") 
-default:
-    print("Opção invalida!")
-    
-}
-
-//Atividade de entrega
-
-import Foundation
-
-print("Informe sua idade:")
-let idade = Int(readLine()!)!
-
-print("Informe sua altura (em metros):")
-let altura = Double(readLine()!)!
-
-print("Está acompanhado? (sim/não)")
-let entrada = readLine()!.lowercased()
-let acompanhado = (entrada == "sim")
-
-if idade >= 12 && altura >= 1.40 {
-    print("Entrada liberada! Tudo certo com sua idade e altura.")
-} else if idade == 10 && acompanhado {
-    print("Entrada liberada! Tem 10 anos e está acompanhado.")
-} else {
-    print("Acesso negado! Lamento precisa de autorização.")
-}
-
+print(String(format: "A=%.4f\n", area))
 
