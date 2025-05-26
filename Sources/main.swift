cff947a70f041177de11111b13cbfea87f9b27da
CleideLustosa <142410141+CleideLustosa@users.noreply.github.com>
2025-05-26 01:37:37 +0000

    Atividade01 - Exerc05

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,8 +1,35 @@
 import Foundation
 
-let a = Double(readLine()!)!
-let b = Double(readLine()!)!
+var a: Double = -1
+var b: Double = -1
 
-let media = (a * 3.5 + b * 7.5) / 11
+while a < 0 || a > 10 {
+    print("Digite a nota A:")
+    if let entrada = readLine(), let nota = Double(entrada) {
+        if nota >= 0 && nota <= 10 {
+            a = nota
+        } else {
+            print("Nota inválida. Digite um valor entre 0 e 10.")
+        }
+    } else {
+        print("Entrada inválida. Digite um número.")
+    }
+}
+
+while b < 0 || b > 10 {
+    print("Digite a nota B:")
+    if let entrada = readLine(), let nota = Double(entrada) {
+        if nota >= 0 && nota <= 10 {
+            b = nota
+        } else {
+            print("Nota inválida. Digite um valor entre 0 e 10.")
+        }
+    } else {
+        print("Entrada inválida. Digite um número.")
+    }
+}
+
+let media = (a * 3.5 + b * 7.5) / 11.0
 print(String(format: "MEDIA = %.5f", media))
+
 
