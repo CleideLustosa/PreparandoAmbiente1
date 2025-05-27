71df3a3599f405eeae7e9f7b0270017a8def9539
CleideLustosa <142410141+CleideLustosa@users.noreply.github.com>
2025-05-27 18:51:44 +0000

    Aula 6 - Array

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,34 +1,16 @@
 import Foundation
 
-print("Digite o código do produto:")
-let codigo = Int(readLine()!)!
-
-print("Digite a quantidade:")
-let quantidade = Int(readLine()!)!
-
-var preco: Double
-
-switch codigo {
-case 1:
-    preco = 4.00
-case 2:
-    preco = 4.50
-case 3:
-    preco = 5.00
-case 4:
-    preco = 2.00
-case 5:
-    preco = 1.50
-default:
-    preco = 0.0
+func somaElementos(array: [Int]) -> Int{
+    var soma = 0
+    for num in array {
+        soma += num   
+    }
+    return soma
 }
 
-if preco > 0 {
-    let total = preco * Double(quantidade)
-    print("Total: R$ \(String(format: "%.2f", total))")
-} else {
-    print("Código inválido.")
-}
+let numeros = [1,2,3]
+print(somaElementos(array: numeros))
+
 
 
 
