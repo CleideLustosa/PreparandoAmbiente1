9114e85714848a093669489532efbde8892af56c
CleideLustosa <142410141+CleideLustosa@users.noreply.github.com>
2025-05-30 02:38:10 +0000

    projeto total

--- Sources/main.swift
+++ Sources/main.swift
@@ -52,6 +52,49 @@ func removerTarefa(tarefas: [String]) -> [String] {
     return lista
 }
 
+func alterarTarefa(tarefas: [String]) -> [String] {
+    var lista = tarefas
+    if lista.isEmpty {
+        print("Não há tarefas para alterar.")
+        return lista
+    }
+
+    print("Tarefas atuais:")
+    var contador = 1
+    for tarefa in lista {
+        print("\(contador). \(tarefa)")
+        contador += 1
+    }
+
+    print("Digite o número da tarefa para alterar:")
+    if let entrada = readLine(), let numero = Int(entrada), numero > 0, numero <= lista.count {
+        print("Digite a nova tarefa:")
+        if let novaTarefa = readLine(), !novaTarefa.isEmpty {
+            lista[numero - 1] = novaTarefa
+            print("Tarefa alterada com sucesso!")
+        } else {
+            print("Texto inválido.")
+        }
+    } else {
+        print("Número inválido.")
+    }
+
+    return lista
+}
+
+func visualizarTarefas(tarefas: [String]) {
+    if tarefas.isEmpty {
+        print("Nenhuma tarefa cadastrada.")
+    } else {
+        print("Lista de Tarefas:")
+        var contador = 1
+        for tarefa in tarefas {
+            print("\(contador). \(tarefa)")
+            contador += 1
+        }
+    }
+}
+
 // variaveis e while 
 
 var listaDeTarefas: [String] = []  
@@ -66,6 +109,10 @@ while menuAberto {
             listaDeTarefas = adicionarTarefa(tarefa: listaDeTarefas)
         case "2":
             listaDeTarefas = removerTarefa(tarefas: listaDeTarefas)
+         case "3":
+            listaDeTarefas = alterarTarefa(tarefas: listaDeTarefas)
+        case "4":
+            visualizarTarefas(tarefas: listaDeTarefas) 
         case "5":
             print("Sair")
             menuAberto = false
