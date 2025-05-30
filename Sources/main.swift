5623f163657f2ea6d46111445c16ee99b3442662
CleideLustosa <142410141+CleideLustosa@users.noreply.github.com>
2025-05-30 02:14:45 +0000

    Funcoes: Add, removr e mostrar menu completa

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,59 +1,76 @@
 import Foundation
 
-
-var tarefa: [String] = []
-
-func mostrarMenu(){
+func mostrarMenu() {
     print("""
     Menu To-Do List
     1. Adicionar Tarefa
     2. Remover Tarefa
     3. Alterar Tarefa
-    4. Vizualiazar Tarefa
+    4. Visualizar Tarefa
     5. Sair
-    Escolha uma opção.
+    Escolha uma opção:
     """)
 }
 
-func adicionarTarefa(tarefa: [String])-> String{
+func adicionarTarefa(tarefa: [String]) -> [String] {
     var lista = tarefa
+
     print("Digite sua tarefa: ")
-    if let novaTarefa = readLine(), !novaTarefa.isEmpty{
+    if let novaTarefa = readLine(), !novaTarefa.isEmpty {
         lista.append(novaTarefa)
-        print("Tarefa adiconada com sucesso!")        
-    }else {
+        print("Tarefa adicionada com sucesso!")        
+    } else {
         print("Tarefa inválida.")
-        
     } 
-    return lista 
 
+    return lista
 }
 
-// criar variavel 
-let entrada = readLine()!
-var addTarefa = "Estudar"
-
-
-while (addTarefa){
-    addTarefa = String(entrada)!
-    print("Tarefa Adicionada")
-}else {
-    print("Sair")
+func removerTarefa(tarefas: [String]) -> [String] {
+    var lista = tarefas
     
-}
-
-tarefa = adicionarTarefa(tarefa:tarefa)   
-
-func removerTarefa(tarefa: [String]) -> String {
-    visualizarTarefa(lista)
-    print("Escolha a tarefa para remover."){
-        for in tarefa{
-            
-        }
+    if lista.isEmpty {
+        print("Não existem tarefas para remover.")
+        return lista
     }
 
+    print("Tarefas:")
+    var contador = 1
+    for tarefa in lista {
+        print("\(contador). \(tarefa)")
+        contador += 1
+    }
+
+    print("Digite o número da tarefa para remover:")
+    if let entrada = readLine(), let numero = Int(entrada), numero > 0, numero <= lista.count {
+        lista.remove(at: numero - 1)
+        print("Tarefa removida com sucesso!")
+    } else {
+        print("Número inválido.")
+    }
+
+    return lista
 }
 
+// variaveis e while 
 
+var listaDeTarefas: [String] = []  
+var menuAberto = true             
 
-
+while menuAberto {
+    mostrarMenu()
+    
+    if let escolha = readLine() {
+        switch escolha {
+        case "1":
+            listaDeTarefas = adicionarTarefa(tarefa: listaDeTarefas)
+        case "2":
+            listaDeTarefas = removerTarefa(tarefas: listaDeTarefas)
+        case "5":
+            print("Sair")
+            menuAberto = false
+        default:
+            print("Opção inválida.")
+        }
+    }
+}
